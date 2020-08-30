61caff66d72b0d1d32d0f6857071194d8d6e7a63
BJS324MC <67012179+BJS324MC@users.noreply.github.com>
2020-08-30 09:19:01 +0000

    Displaying the game
    for nothing

---
+++ Game.js
@@ -0,0 +1,27 @@
+const options=()=>
+    game.functionTimer(
+        [20,120,300,500,200,1200,1000,1000],
+        [
+        ()=>game.spawnEntity('Bullet'),
+        ()=>game.spawnEntity('Homer'),
+        ()=>game.spawnEntity('Line'),
+        ()=>game.spawnEntity('Crusher'),
+        ()=>{
+            let d=game.spawnEntity('Bomb'); 
+            /*d.onExplode=()=>{
+                for(let i in range(8)){
+                    pat2(d.x,d.y,'Bomb',1+i*45);
+                };
+            };*/
+            d.onExplode=()=>pat(d.x,d.y,'Bullet',8);
+            d.timer=50;
+            d.maxSize=2;
+            d.spdtimer=5;
+            },
+        ()=>game.spawnEntity('Bomb'),
+        ()=>game.spawnEntity('Ball'),
+        ()=>game.spawnEntity('HealLine')
+        ]
+    );
+start();
+setInterval(options,0);
